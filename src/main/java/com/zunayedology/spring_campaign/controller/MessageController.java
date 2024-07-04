e860768f8cfcd7cc23291776f903c1a0e2a37694
Zunayed <zunayedology@gmail.com>
2024-07-04 15:01:02 +0600

    CrossOrigin added with WebConfig class

--- src/main/java/com/zunayedology/spring_campaign/controller/MessageController.java
+++ src/main/java/com/zunayedology/spring_campaign/controller/MessageController.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/messages")
+@CrossOrigin(origins = "http://localhost:3000")
 public class MessageController {
     private final MessageService messageService;
 
