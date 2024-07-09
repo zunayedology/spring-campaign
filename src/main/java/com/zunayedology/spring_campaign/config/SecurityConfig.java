77b347aec278ec9bd26f11c0bd879eee97f62713
Zunayed <zunayedology@gmail.com>
2024-07-09 11:15:47 +0600

    In-memory user and admin added

--- src/main/java/com/zunayedology/spring_campaign/config/SecurityConfig.java
+++ src/main/java/com/zunayedology/spring_campaign/config/SecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
@@ -25,6 +27,14 @@ public class SecurityConfig {
 
     @Bean
     public UserDetailsService userDetailsService() {
-        return new InMemoryUserDetailsManager();
+        UserDetails user1 = User.withUsername("user1")
+                .password("{noop}user1Pass")
+                .roles("USER")
+                .build();
+        UserDetails admin = User.withUsername("admin")
+                .password("{noop}adminPass")
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user1, admin);
     }
 }
